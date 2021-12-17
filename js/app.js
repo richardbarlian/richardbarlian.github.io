e63fc73389a48ed506f5547bc98b49324b3d0f21
richardbarlian <89553931+richardbarlian@users.noreply.github.com>
2021-12-17 14:23:26 +0800

    Made it so it will close the dropdown hamburger menu when an item in the dropdown is clicked

--- js/app.js
+++ js/app.js
@@ -17,3 +17,7 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active')
 })
+
+$(".navbar-links").click(function(){
+    navbarLinks.classList.toggle('active')
+});
